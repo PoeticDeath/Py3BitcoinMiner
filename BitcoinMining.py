f01993680b49cec4f424201044c85a8009213b04
PoeticDeath <46275713+PoeticDeath@users.noreply.github.com>
2021-04-03 18:20:34 -0400

    Update BitcoinMining.py

--- BitcoinMining.py
+++ BitcoinMining.py
@@ -3,13 +3,13 @@ def mine(start, cores, ans, cur):
         import hashlib, struct
         nonce = start
         while nonce < 0x100000000:
-            while ans[1] == -1:
-                header = ( struct.pack("<L", ver) + bytes.fromhex(prev_block)[::-1] + bytes.fromhex(mrkl_root)[::-1] + struct.pack("<LLL", time_, bits, nonce))
-                hash = hashlib.sha256(hashlib.sha256(header).digest()).digest()
+            header = ( struct.pack("<L", ver) + bytes.fromhex(prev_block)[::-1] + bytes.fromhex(mrkl_root)[::-1] + struct.pack("<LLL", time_, bits, nonce))
+            hash = hashlib.sha256(hashlib.sha256(header).digest()).digest()
+            if nonce % 10000 == 0:
                 cur[1] = nonce
-                if hash[::-1] < target_str:
-                    ans[1] = nonce
-                nonce += cores
+            if hash[::-1] < target_str:
+                ans[1] = nonce
+            nonce += cores
     except:
         exit()
 def cored_miner():
@@ -24,7 +24,7 @@ def cored_miner():
     start = time()
     n = 0
     while n < cpu_count()*2:
-        Process(target=mine, args=(n, cpu_count()*2, ans, cur,)).start()
+        Process(target=mine, args=(n, cpu_count()*2, ans, cur,), daemon=True).start()
         n += 1
     while ans[1] == -1:
         print(f'{cur[1]:,}', f'{int(cur[1] / (time() - start)):,}', 'H/s' , end='\r')
