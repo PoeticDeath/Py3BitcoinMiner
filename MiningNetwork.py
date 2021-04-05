5ee37b8b9c7a563d445e4139a2a16bf508cdd931
PoeticDeath <46275713+PoeticDeath@users.noreply.github.com>
2021-04-05 10:29:30 -0400

    Update MiningNetwork.py

--- MiningNetwork.py
+++ MiningNetwork.py
@@ -7,32 +7,39 @@ import subprocess
 import os
 manager = Manager()
 ans = manager.dict()
-while True:
-    start = time()
-    ans[1] = -1
-    old_block = blockcypher.get_latest_block_hash()
-    prev_block = old_block
-    ver = 1
-    time_ = int(time())
-    s = subprocess.check_output("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli getmininginfo", shell=True)
-    s = str(str(s)).replace("b\'", "")
-    s = str(str(s)).replace("\'", "")
-    s = str(str(s)).replace("\\n", "")
-    s = literal_eval(s)
-    mrkl_root =
-    target_str = hex(s['difficulty'] * 2**(8*(0x1b -3)))[2:]
-    while len(target_str) < 64:
-        target_str = "0" + target_str
-    target_str = int("0x" + target_str, 16)
-    bits = blockcypher.get_blockchain_overview(old_block)['bits']
-    P = Process(target=cored_miner, args=(ans,))
-    P.start()
-    while prev_block == old_block:
-        prev_block = blockcypher.get_latest_block_hash()
-        if ans[1] != -1:
-            os.system("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli submitheader " + ans[1])
-            print("Successfully solved block", str(blockcypher.get_latest_block_height(old_block)+1), "in", str(time() - start), "seconds.")
-            break
-    P.terminate()
-    if ans[1] == -1:
-        print("Didn't solve block", str(blockcypher.get_latest_block_height(old_block)+1), "in time, lasted", str(time() - start), "seconds.")
+def Bitcoind():
+    os.system("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoind -prune=550 -wallet=PoeticDeath")
+B = Process(target=Bitcoind)
+B.start()
+try:
+    while True:
+        start = time()
+        ans[1] = -1
+        old_block = blockcypher.get_latest_block_hash()
+        prev_block = old_block
+        ver = 1
+        time_ = int(time())
+        s = subprocess.check_output("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli getmininginfo", shell=True)
+        s = str(str(s)).replace("b\'", "")
+        s = str(str(s)).replace("\'", "")
+        s = str(str(s)).replace("\\n", "")
+        s = literal_eval(s)
+        mrkl_root =
+        target_str = hex(s['difficulty'] * 2**(8*(0x1b -3)))[2:]
+        while len(target_str) < 64:
+            target_str = "0" + target_str
+        target_str = int("0x" + target_str, 16)
+        bits = blockcypher.get_blockchain_overview(old_block)['bits']
+        P = Process(target=cored_miner, args=(ans,))
+        P.start()
+        while prev_block == old_block:
+            prev_block = blockcypher.get_latest_block_hash()
+            if ans[1] != -1:
+                os.system("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli submitheader " + ans[1])
+                print("Successfully solved block", str(blockcypher.get_latest_block_height(old_block)+1), "in", str(time() - start), "seconds.")
+                break
+        P.terminate()
+        if ans[1] == -1:
+            print("Didn't solve block", str(blockcypher.get_latest_block_height(old_block)+1), "in time, lasted", str(time() - start), "seconds.")
+except:
+    B.terminate()
