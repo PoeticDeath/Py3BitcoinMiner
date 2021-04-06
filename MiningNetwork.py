0d422c941e9c81738a018e518746163a50975d83
PoeticDeath <46275713+PoeticDeath@users.noreply.github.com>
2021-04-05 22:48:37 -0400

    Update MiningNetwork.py

--- MiningNetwork.py
+++ MiningNetwork.py
@@ -9,6 +9,8 @@ manager = Manager()
 ans = manager.dict()
 def Bitcoind():
     os.system("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoind -prune=550 -wallet=PoeticDeath")
+def dblsha(data):
+ 	return hashlib.sha256(hashlib.sha256(data).digest()).digest()
 B = Process(target=Bitcoind)
 B.start()
 try:
@@ -18,18 +20,29 @@ try:
         old_block = blockcypher.get_latest_block_hash()
         prev_block = old_block
         ver = 1
-        time_ = int(time())
         s = subprocess.check_output("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli getmininginfo", shell=True)
         s = str(str(s)).replace("b\'", "")
         s = str(str(s)).replace("\'", "")
         s = str(str(s)).replace("\\n", "")
         s = literal_eval(s)
-        mrkl_root =
+        r = subprocess.check_output("/Programs/Bitcoin/bitcoin-0.21.0/bin/bitcoin-cli getblocktemplate {'\"rules\": [\"segwit\"]'}", shell=True)
+        r = str(str(r)).replace("b\'", "")
+        r = str(str(r)).replace("\'", "")
+        r = str(str(r)).replace("\\n", "")
+        r = literal_eval(r)
+        txnlist = [binascii.a2b_hex(a['data']) for a in r['transactions']]
+        merklehashes = [dblsha(t) for t in txnlist]
+        while len(merklehashes) > 1:
+	           if len(merklehashes) % 2:
+	  	               merklehashes.append(merklehashes[-1])
+ 	           merklehashes = [dblsha(merklehashes[i] + merklehashes[i + 1]) for i in range(0, len(merklehashes), 2)]
+        mrkl_root = merklehashes[0]
+        time_ = r['curtime']
+        bits = r['bits']
         target_str = hex(s['difficulty'] * 2**(8*(0x1b -3)))[2:]
         while len(target_str) < 64:
             target_str = "0" + target_str
         target_str = int("0x" + target_str, 16)
-        bits = blockcypher.get_blockchain_overview(old_block)['bits']
         P = Process(target=cored_miner, args=(ans,))
         P.start()
         while prev_block == old_block:
