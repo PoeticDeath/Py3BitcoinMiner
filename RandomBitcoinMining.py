3fba26552fdab4e6bdb7a8869860052b57bcd5fe
PoeticDeath <46275713+PoeticDeath@users.noreply.github.com>
2021-04-07 12:38:49 -0400

    Update RandomBitcoinMining.py

--- RandomBitcoinMining.py
+++ RandomBitcoinMining.py
@@ -5,7 +5,7 @@ def mine(cores, ans, cur, ver, prev_block, mrkl_root, time_, bits, target_str):
         nonce = randint(0, 4294967297)
         while True:
             count = 0
-            while count < 0x100000000:
+            while count < 2000000:
                 if ans[3] and ans[4] == 0:
                     exit()
                 header = ( struct.pack("<L", ver) + bytes.fromhex(prev_block)[::-1] + bytes.fromhex(mrkl_root)[::-1] + struct.pack("<LLL", time_, bits, nonce))
